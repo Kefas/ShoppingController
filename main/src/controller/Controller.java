50a26b6c3892610f43c6e0589471372f7a2dd3db
piotr <piotrkonsek@gmail.com>
2014-08-09 19:03:21 +0200

    price on click controller

--- main/src/controller/Controller.java
+++ main/src/controller/Controller.java
@@ -2,6 +2,8 @@ package controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import model.Model;
 import view.AddPanel;
@@ -27,6 +29,36 @@ public class Controller {
 
 	protected void handlerBtnAdd() {
 		view.setChildPanel(new AddPanel());
+		((AddPanel)view.getChildPanel()).getTxtPrice().addMouseListener(new MouseListener() {
+			
+			@Override
+			public void mouseReleased(MouseEvent e) {				
+			}
+			
+			@Override
+			public void mousePressed(MouseEvent e) {			
+			}
+			
+			@Override
+			public void mouseExited(MouseEvent e) {				
+			}
+			
+			@Override
+			public void mouseEntered(MouseEvent e) {			
+			}
+			
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				handlerAddPriceText();
+			}
+		});
+	}
+
+	protected void handlerAddPriceText() {
+		if(((AddPanel)view.getChildPanel()).isFirstClickTxtPrice()){
+			((AddPanel)view.getChildPanel()).getTxtPrice().setText("");
+			((AddPanel)view.getChildPanel()).setFirstClickTxtPrice(false);
+		}
 	}
 
 }
